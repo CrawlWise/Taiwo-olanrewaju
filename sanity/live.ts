39a113db7d617fc0847e70bc124ac881b9e9bc38
Adejorin Ayomiposi <adejorinayomiposi@gmail.com>
2026-06-17 10:43:51 +0100

    Fix books page

--- sanity/live.ts
+++ sanity/live.ts
@@ -1,5 +1,11 @@
 import { client } from "./client";
+import type { QueryParams } from "@sanity/client";
 
-export async function sanityFetch(query: string) {
-  return client.fetch(query, {}, { cache: "no-store" });
+export async function sanityFetch<T = unknown>(
+  query: string,
+  params: QueryParams = {}
+): Promise<T> {
+  return client.fetch(query, params, {
+    cache: "no-store",
+  });
 }
